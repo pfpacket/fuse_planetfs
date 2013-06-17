c3ef797dd0a47b7d0f3b2bf93c5718ddffbfae42
pf packet <afpacket@gmail.com>
2013-06-17 13:35:34 +0900

    operation_layer: separate definition from decl

--- include/planet/operation_layer.hpp
+++ include/planet/operation_layer.hpp
@@ -3,34 +3,18 @@
 
 #include <planet/common.hpp>
 #include <planet/planet_handle.hpp>
-#include <planet/fs_core.hpp>
 
 namespace planet {
 
 
-    int read(handle_t handle, char *buf, size_t size, off_t offset)
-    {
-        auto& op_tuple = handle_mgr.get_operation_entry(handle);
-        return std::get<1>(op_tuple)->read(
-            std::get<0>(op_tuple), buf, size, offset
-        );
-    }
+    // read(2) system call entry
+    extern int read(handle_t, char *, size_t, off_t);
 
-    int write(handle_t handle, char const *buf, size_t size, off_t offset)
-    {
-        auto& op_tuple = handle_mgr.get_operation_entry(handle);
-        return std::get<1>(op_tuple)->write(
-            std::get<0>(op_tuple), buf, size, offset
-        );
-    }
+    // write(2) system call entry
+    extern int write(handle_t, char const *, size_t, off_t);
 
-    int close(handle_t handle)
-    {
-        auto& op_tuple = handle_mgr.get_operation_entry(handle);
-        int ret = std::get<1>(op_tuple)->release(std::get<0>(op_tuple));
-        handle_mgr.unregister_op(handle);
-        return ret;
-    }
+    // close(2) system call entry
+    extern int close(handle_t);
 
 
 }   // namespace planet
---
+++ src/planet/operation_layer.cpp
@@ -0,0 +1,33 @@
+
+#include <planet/operation_layer.hpp>
+#include <planet/fs_core.hpp>
+
+namespace planet {
+
+
+    int read(handle_t handle, char *buf, size_t size, off_t offset)
+    {
+        auto& op_tuple = handle_mgr.get_operation_entry(handle);
+        return std::get<1>(op_tuple)->read(
+            std::get<0>(op_tuple), buf, size, offset
+        );
+    }
+
+    int write(handle_t handle, char const *buf, size_t size, off_t offset)
+    {
+        auto& op_tuple = handle_mgr.get_operation_entry(handle);
+        return std::get<1>(op_tuple)->write(
+            std::get<0>(op_tuple), buf, size, offset
+        );
+    }
+
+    int close(handle_t handle)
+    {
+        auto& op_tuple = handle_mgr.get_operation_entry(handle);
+        int ret = std::get<1>(op_tuple)->release(std::get<0>(op_tuple));
+        handle_mgr.unregister_op(handle);
+        return ret;
+    }
+
+
+}   // namespace planet
