8b9df33e63098bbf60ecc60755715a20a0058c43
pf packet <afpacket@gmail.com>
2014-01-04 02:50:27 +0900

    Simplify example/simple_http_client.py

--- example/simple_http_client.py
+++ example/simple_http_client.py
@@ -5,47 +5,26 @@
 #
 import sys
 
-class raw_io:
-    def __init__(self, *args, **kwargs):
-        self.open_(*args, **kwargs)
-    @staticmethod
-    def open(*args, **kwargs):
-        return raw_io(*args, **kwargs)
-    def open_(self, filename, mode, *args, **kwargs):
-        self.file = open(filename, mode + "b", buffering=0, *args, **kwargs)
-    def read(self, *args, **kwargs):
-        return self.file.read(*args, **kwargs).decode()
-    def write(self, buf, *args, **kwargs):
-        return self.file.write(buf.encode(), *args, **kwargs)
-    def close(self, *args, **kwargs):
-        return self.file.close(*args, **kwargs)
-    def get(self):
-        return self.file
+def simple_http_client(hostname, path):
+    dns = open("/net/dns", "rb+", buffering=0)
+    dns.write(("resolve_inet " + hostname).encode())
+    resolved = dns.read().decode().splitlines()
 
-def resolve_name(hostname):
-    dns = raw_io.open("/net/dns", "r+")
-    dns.write("resolve_inet " + hostname)
-    return dns.read().splitlines()
+    tcp_clone = open("/net/tcp/clone", "rb", buffering=0)
+    clone_num = tcp_clone.read().decode()
 
-def simple_http_client(hostname):
-    resolved = resolve_name(hostname)
+    tcp_ctl = open("/net/tcp/" + clone_num + "/ctl", "wb", buffering=0)
+    tcp_ctl.write("connect {0}!80".format(resolved[0]).encode())
 
-    tcp_clone = raw_io.open("/net/tcp/clone", "r")
-    clone_num = tcp_clone.read()
-    tcp_ctl_path = "/net/tcp/" + clone_num + "/ctl"
-
-    tcp_ctl = raw_io.open(tcp_ctl_path, "w")
-    tcp_ctl.write("connect " + resolved[0] + "!80")
-
-    tcp_data = raw_io.open("/net/tcp/" + clone_num + "/data", "r+")
-    tcp_data.write(
-        "GET /index.html HTTP/1.0\r\nHost: {0}\r\n\r\n"
-    .format(hostname))
-    http_response = tcp_data.get().read()
-    tcp_ctl.write("hangup")
+    tcp_data = open("/net/tcp/" + clone_num + "/data", "rb+", buffering=0)
+    tcp_data.write("GET {0} HTTP/1.1\r\nHost: {1}\r\n\r\n".format(path, hostname).encode())
+    http_response = tcp_data.read()
+    tcp_ctl.write("hangup".encode())
     return http_response
 
 if __name__ == "__main__":
+    if (len(sys.argv) < 3):
+        sys.exit("Usage: ./{0} HOSTNAME [PATH]".format(sys.argv[0]))
     sys.stdout.buffer.write(simple_http_client(
-        "www.google.com" if len(sys.argv) < 2 else sys.argv[1]
+        hostname=sys.argv[1], path=sys.argv[2] if len(sys.argv) > 2 else "/"
     ))
