484ddc127d64168b71f924142e9df7ee1146ec6c
pf packet <afpacket@gmail.com>
2014-01-04 20:50:30 +0900

    Update example/simple_http_client.py

--- example/simple_http_client.py
+++ example/simple_http_client.py
@@ -17,14 +17,16 @@ def simple_http_client(hostname, path):
     tcp_ctl.write("connect {0}!80".format(resolved[0]).encode())
 
     tcp_data = open("/net/tcp/" + clone_num + "/data", "rb+", buffering=0)
-    tcp_data.write("GET {0} HTTP/1.1\r\nHost: {1}\r\n\r\n".format(path, hostname).encode())
+    tcp_data.write(
+        "GET {0} HTTP/1.1\r\nHost: {1}\r\nConnection: close\r\n\r\n"
+    .format(path, hostname).encode())
     http_response = tcp_data.read()
     tcp_ctl.write("hangup".encode())
     return http_response
 
 if __name__ == "__main__":
-    if (len(sys.argv) < 3):
+    if (len(sys.argv) < 2):
         sys.exit("Usage: ./{0} HOSTNAME [PATH]".format(sys.argv[0]))
     sys.stdout.buffer.write(simple_http_client(
-        hostname=sys.argv[1], path=sys.argv[2] if len(sys.argv) > 2 else "/"
+        hostname=sys.argv[1], path=(sys.argv[2] if len(sys.argv) > 2 else "/")
     ))
